6ca7a108acb6b04153b15e56c60219cd56d8490d
Stephen <37083603+st7ma784@users.noreply.github.com>
2024-03-26 11:21:43 +0000

    Update DataModule.py

--- DataModule.py
+++ DataModule.py
@@ -8,46 +8,39 @@ from pySmartDL import SmartDL
 import pytorch_lightning as pl
 from torch.utils.data import ConcatDataset
 from torchvision.datasets import CocoCaptions
-T= transforms.Compose([transforms.Resize((224,224),interpolation=Image.NEAREST),transforms.ToTensor()])
-from transformers import AutoTokenizer
-import time
-tokenizer= AutoTokenizer.from_pretrained("gpt2")
-tokenizer.vocab["</s>"] = tokenizer.vocab_size -1
-tokenizer.pad_token = tokenizer.eos_token 
+from scipy.optimize import linear_sum_assignment
+import logging
 
+from functools import partial
+def outputconversion(func): #converts the output of a function back to 1-hot tensor
+    def wrapper(*args, **kwargs):
+        func=kwargs.pop("func")
+        args=list(args)
+        x=args.pop(0)
+        output=torch.zeros_like(x)
 
+        x1,y1=func(x, *args, **kwargs)
+        try:
+            output[x1,y1]=1
+        except:
+            output[y1,x1]=1
+        return output
+
+LSA=outputconversion(linear_sum_assignment)
 ## An Example dataset, needs to implement a torch.data.utils.Dataset. This one automatically loads COCO for us from MSCOCO annotations, which we extend to include our own tokenizer
 
-class myDataset(CocoCaptions):
-    def __init__(self, root, annFile, *args, **kwargs):
+class myDataset(torch.utils.data.Dataset):
+    def __init__(self, size=(5,10),*args, **kwargs):
         print('Loading COCO dataset')
         #check if root and annfile exist
-        if not os.path.exists(root):
-            print('Error: root directory does not exist: {}'.format(root))
-            return None
-        if not os.path.exists(annFile):
-            print('Error: annFile does not exist: {}'.format(annFile))
-            return None
-        #if using the HEC, you may want a Path(annFile).touch() call here to update the altered time if your files are stored in $global_scratch
-        super().__init__(root, annFile, *args, **kwargs)
-        print('Done')
+        self.w,self.h=size
+        super().__init__( *args, **kwargs)
+    def __len__(self):
+        return 10000
     def __getitem__(self, index: int):
-        try:
-            img, target= super().__getitem__(index)
-        except Exception as e:
-            print(e)
-            print('Error loading image:', index)
-            return None
-        target=torch.cat([tokenizer(
-                    sent,                      # Sentence to encode.
-                    add_special_tokens = True, # Add '[CLS]' and '[SEP]'
-                    max_length = 77,           # Pad & truncate all sentences.
-                    padding = "max_length",
-                    truncation=True,
-                    return_attention_mask = False,   # Construct attn. masks.
-                    return_tensors = 'pt',     # Return pytorch tensors.
-                )['input_ids'] for sent in target[:5]],dim=0)
-        return img,target
+        array=torch.rand(self.w,self.h)
+        truth=LSA(array)
+        return array,truth
 
 
 # Dataset
@@ -90,120 +83,8 @@ class myDataModule(pl.LightningDataModule):
 
         return torch.utils.data.DataLoader(self.test, batch_size=self.batch_size, shuffle=True, num_workers=4, prefetch_factor=4, pin_memory=True,drop_last=True)
     def prepare_data(self):
-        '''called only once and on 1 GPU'''
-        # # download data
-        #If using the HEC, consider altering this to call directly the cmdline WGET/CURL  and then unzip -DD to modify the dates. 
-        
-        if not os.path.exists(self.data_dir):
-            os.makedirs(self.data_dir,exist_ok=True)
-        if not os.path.exists(self.ann_dir):
-            os.makedirs(self.ann_dir,exist_ok=True)
-        urls=['http://images.cocodataset.org/zips/train2014.zip',
-                'http://images.cocodataset.org/zips/val2014.zip',
-                'http://images.cocodataset.org/zips/test2015.zip',
-                'http://images.cocodataset.org/zips/train2017.zip',
-                'http://images.cocodataset.org/zips/val2017.zip',
-                'http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
-                'http://images.cocodataset.org/annotations/annotations_trainval2017.zip'
-                ]
-
-        objs=[]
-        for url in urls:
-            print("url:",url)
-            name=str(url).split('/')[-1]
-        
-            
-            location=self.data_dir # if name.startswith("annotations") else self.ann_dir
-            #print("Location", location) #/Data/train2014.zip
-            #time.sleep(5)
-            #print('Downloading',url)
-            if name.endswith(".zip"):
-                name=name[:-4]
-            if name.startswith("train"):
-                self.splits['train'].append(name)
-            elif name.startswith("val"):
-                self.splits['val'].append(name)
-            elif name.startswith("test"):
-                self.splits['test'].append(name)
-            obj=SmartDL(url,os.path.join(location,str(url).split('/')[-1]),progress_bar=False)
-            obj.FileName=name
-            if not os.path.exists(obj.get_dest()):
-
-                objs.append(obj)#SmartDL(url, self.data_dir,)
-                obj.start(blocking=False)
-                print("obj Path ",obj.get_dest())
-        for obj in objs:
-            while not obj.isFinished():
-                #print("Speed: %s" % obj.get_speed(human=True))
-                print("Eta: %s" % obj.get_eta(human=True))
-                time.sleep(5)
-            if obj.isSuccessful():
-                print("Downloaded: %s" % obj.get_dest())
-
-            path = obj.get_dest()
-            if obj.FileName.startswith("annotations"):
-                print("Extracting annotations")
-                print("path:",path)
-
-                with zipfile.ZipFile(path, 'r') as zip_ref:
-                    try:
-                        zip_ref.extractall(self.data_dir)
-                    except:
-                        print("Error extracting annotations")
-                        print("path:",path)
-                        print("ann_dir:",self.ann_dir)
-            #wget.download("http://images.cocodataset.org/zips/train2014.zip",out=self.cocodir)
-            else:
-                print("Extracting images")
-                print("path:",path)
-                if obj.FileName.endswith(".zip"):
-                    print("Extracting zip")
-                    with zipfile.ZipFile(path, 'r') as zip_ref:
-                        try:
-                            zip_ref.extractall(self.data_dir)
-                        except:
-                            print("Error extracting images")
-                            print("path:",path)
-                            print("data_dir:",self.data_dir)
-                print("Extracted: %s" % path)
-
- 
+      
     def setup(self, stage=None):
         '''called on each GPU separately - stage defines if we are at fit or test step'''
         print("Entered COCO datasetup")
-        if stage == 'fit' or stage is None:
-            TrainSets=[]
-            for version in self.splits['train']:
-                
-                annfile=os.path.join(self.ann_dir,'{}_{}.json'.format('captions',version))
-                dir=os.path.join(self.data_dir,version)
-
-                #time.sleep(2)
-                dset=myDataset(root=dir, annFile=annfile, transform=self.T)
-                assert(len(dset)>0)
-                TrainSets.append(dset)
-            self.train = ConcatDataset(TrainSets)
-
-            ValSets=[]
-            for version in self.splits['val']:
-                print("BUILDING SPLIT : ",version)
-                
-                annfile=os.path.join(self.ann_dir,'{}_{}.json'.format('captions',version))
-                dir=os.path.join(self.data_dir,version)
-                print("annfile:",annfile)
-                print("dir:",dir)
-                ValSets.append(myDataset(root=dir, annFile=annfile, transform=self.T))
-            self.val = ConcatDataset(ValSets)
-            # torch.save(self.train,"train.pt")
-            # torch.save(self.val,"val.pt")    
-        if stage == 'test' or stage is None:
-            TestSets=[]
-            for version in self.splits['test']:
-                print("BUILDING SPLIT : ",version)
-                annfile=os.path.join(self.ann_dir,'{}_{}.json'.format('captions',version))
-                dir=os.path.join(self.data_dir,version)
-                
-                print("annfile:",annfile)
-                print("dir:",dir)
-                TestSets.append(myDataset(root=dir, annFile=annfile, transform=self.T))
-            self.test = ConcatDataset(TestSets)
+       
